b73c7c03995f441e004145c9eed3e5da59c6b775
william-hayward <5hayww75@solent.ac.uk>
2022-01-19 20:43:03 +0000

    fixing bugs

--- controllers/switch.js
+++ controllers/switch.js
@@ -6,7 +6,7 @@ exports.list = async (req, res) => {
         console.log(req.query)
         const message = req.query.message;
         const games = await Games.find({platform: "Nintendo Switch"});
-        res.render("nintendoswitch", { games: games, message: message });
+        res.render("nintendoSwitch", { games: games, message: message });
     } catch (e) {
         res.status(404).send({ message: "could not list games"});
     }
